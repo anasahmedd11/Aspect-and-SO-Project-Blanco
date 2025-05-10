947b09176b79d99edb7264863b9703603c21c12f
boudyK <abdalrahmank2000@gmail.com>
2025-05-10 22:21:05 +0300

    small typo

--- DatabaseService/src/main/java/com/example/DatabaseService/repository/NotificationsRepository.java
+++ DatabaseService/src/main/java/com/example/DatabaseService/repository/NotificationsRepository.java
@@ -3,4 +3,4 @@ package com.example.DatabaseService.repository;
 import com.example.DatabaseService.entity.Notifications;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationsRepository extends JpaRepository<Notifications, Integer> {}
+public interface NotificationsRepository extends JpaRepository<Notifications, Long> {}
