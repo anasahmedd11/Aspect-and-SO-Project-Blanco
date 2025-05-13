4a2535b2b5d6db3761893dd01cf9e0ab01cb10fb
boudyK <abdalrahmank2000@gmail.com>
2025-05-13 21:07:09 +0300

    adding Expenses service

--- MoneyManagementService/src/main/java/com/example/MoneyManagementService/DTO/ExpenseDTO.java
+++ MoneyManagementService/src/main/java/com/example/MoneyManagementService/DTO/ExpenseDTO.java
@@ -1,4 +1,22 @@
 package com.example.MoneyManagementService.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExpenseDTO {
+
+
+    private Long userId;
+    private Double amount;
+    private Long categoryId;
+    private String notes;
+    private String date;
+
+
 }
---
+++ MoneyManagementService/src/main/java/com/example/MoneyManagementService/service/ExpenseService.java
@@ -0,0 +1,4 @@
+package com.example.MoneyManagementService.service;
+
+public class ExpenseService {
+}
