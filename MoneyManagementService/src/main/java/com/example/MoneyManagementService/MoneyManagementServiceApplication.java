5fc35647793e923dfc6f18e91627d7db8d9a2d29
boudyK <abdalrahmank2000@gmail.com>
2025-05-13 19:32:03 +0300

    adding Money Management Service

---
+++ MoneyManagementService/src/main/java/com/example/MoneyManagementService/MoneyManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MoneyManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagementServiceApplication.class, args);
+	}
+
+}
