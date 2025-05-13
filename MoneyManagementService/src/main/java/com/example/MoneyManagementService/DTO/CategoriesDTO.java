01fe1b4341d90f61c6d36354783f0bc9e3db9e94
Anas Ahmed <anas.a.elyamanyy@gmail.com>
2025-05-13 21:04:05 +0300

    categories fix

---
+++ MoneyManagementService/src/main/java/com/example/MoneyManagementService/DTO/CategoriesDTO.java
@@ -0,0 +1,18 @@
+package com.example.MoneyManagementService.DTO;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriesDTO {
+
+    private String name;
+
+}
---
+++ MoneyManagementService/src/main/java/com/example/MoneyManagementService/entity/Categories.java
@@ -0,0 +1,24 @@
+package com.example.MoneyManagementService.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categories {
+
+    private Long id;
+
+    private String name;
+
+    public Categories(String name) {
+        this.name = name;
+    }
+}
