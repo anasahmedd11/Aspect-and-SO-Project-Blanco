60bdbe00f0d69ae89544b3feb899053c7101d4dd
Anas Ahmed <anas.a.elyamanyy@gmail.com>
2025-05-13 23:12:54 +0300

    added categories controller

---
+++ MoneyManagementService/src/main/java/com/example/MoneyManagementService/Controllers/CategoriesController.java
@@ -0,0 +1,36 @@
+package com.example.MoneyManagementService.Controllers;
+
+import com.example.MoneyManagementService.DTO.CategoriesDTO;
+import com.example.MoneyManagementService.service.CategoriesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/money-management-service/categories")
+public class CategoriesController {
+    private final CategoriesService categoryService;
+
+    @Autowired
+    public CategoriesController(CategoriesService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriesDTO> createCategory(@RequestBody CategoriesDTO categoryDTO) {
+        return ResponseEntity.ok(categoryService.createCategory(categoryDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriesDTO>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+}
