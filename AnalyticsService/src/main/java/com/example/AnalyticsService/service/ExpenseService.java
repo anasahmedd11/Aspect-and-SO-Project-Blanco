12b8d0dbf1fb92d75b5b7abe935c9ce8a455f5c6
yahia zaki <yahia.az.yz@gmail.com>
2025-05-14 22:52:38 +0300

    Analytics service expense service added

--- AnalyticsService/src/main/java/com/example/AnalyticsService/service/ExpenseService.java
+++ AnalyticsService/src/main/java/com/example/AnalyticsService/service/ExpenseService.java
@@ -1,8 +1,16 @@
 package com.example.AnalyticsService.service;
 
+import com.example.AnalyticsService.entity.Expenses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ExpenseService {
@@ -13,6 +21,26 @@ public class ExpenseService {
     @Autowired
     public ExpenseService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+
+    public List<Expenses> getAllUserExpenses(Long userId) {
+        String url = baseurl + "/user/" + userId;
+        ResponseEntity<Expenses[]> response = restTemplate.getForEntity(url, Expenses[].class);
+        return Arrays.asList(response.getBody());
+    }
+
+    public Optional<Expenses> getExpenseById(Long id) {
+        String url = baseurl + "/" + id;
+        try {
+            ResponseEntity<Expenses> response = restTemplate.getForEntity(url, Expenses.class);
+            return Optional.ofNullable(response.getBody());
+        } catch (HttpStatusCodeException e) {
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                return Optional.empty();
+            }
+            throw e;
+        }
     }
 
 
