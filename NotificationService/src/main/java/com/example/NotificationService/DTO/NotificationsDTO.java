7954f1afd0710ae438470a05e8715f6ec219849b
yahia zaki <yahia.az.yz@gmail.com>
2025-05-14 18:20:11 +0300

    notification service DTO added

--- NotificationService/src/main/java/com/example/NotificationService/DTO/NotificationsDTO.java
+++ NotificationService/src/main/java/com/example/NotificationService/DTO/NotificationsDTO.java
@@ -1,4 +1,23 @@
 package com.example.NotificationService.DTO;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationsDTO {
+
+    private long user_id;
+    private String msg;
+    private String type;
+    private String Status;
+    private Date sent_At;
+
 }
