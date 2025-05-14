aa09568231d11f60bd7c2e09f69b0144e814109c
yahia zaki <yahia.az.yz@gmail.com>
2025-05-14 18:07:50 +0300

    notification service entity created

--- NotificationService/src/main/java/com/example/NotificationService/Entity/Notifications.java
+++ NotificationService/src/main/java/com/example/NotificationService/Entity/Notifications.java
@@ -1,4 +1,31 @@
 package com.example.NotificationService.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notifications {
+
+    private long id;
+    private long user_id;
+    private String msg;
+    private String type;
+    private String Status;
+    private Date sent_At;
+
+    public Notifications(long user_id, String msg, String type, String Status, Date sent_At) {
+        this.user_id = user_id;
+        this.msg = msg;
+        this.type = type;
+        this.Status = Status;
+        this.sent_At = sent_At;
+    }
+
 }
