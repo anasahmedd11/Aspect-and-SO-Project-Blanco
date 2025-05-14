d83a5d0969815f410aba765fe0fb553a5f3ff343
boudyK <abdalrahmank2000@gmail.com>
2025-05-14 22:29:20 +0300

    adding Expenses

---
+++ AnalyticsService/src/main/java/com/example/AnalyticsService/entity/Expenses.java
@@ -0,0 +1,42 @@
+package com.example.AnalyticsService.entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expenses {
+
+    private Long id;
+
+
+    private Long userId;
+
+
+
+    private Double amount;
+
+
+    private Long categoryId;
+
+
+    private String notes;
+
+
+
+    private Date date;
+
+
+    public Expenses(Long userId, Double amount, Long categoryId, String notes, Date date) {
+        this.userId = userId;
+        this.amount = amount;
+        this.categoryId = categoryId;
+        this.notes = notes;
+        this.date = date;
+    }
+}
