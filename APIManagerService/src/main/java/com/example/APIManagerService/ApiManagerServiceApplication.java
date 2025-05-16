aeacc895954ca82dd54aa4bdb659d137512942d8
boudyK <abdalrahmank2000@gmail.com>
2025-05-16 15:42:42 +0300

    adding API Manager Service

---
+++ APIManagerService/src/main/java/com/example/APIManagerService/ApiManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIManagerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManagerServiceApplication.class, args);
+	}
+
+}
---
+++ APIManagerService/src/test/java/com/example/APIManagerService/ApiManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIManagerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
