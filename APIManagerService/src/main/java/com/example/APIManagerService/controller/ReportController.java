0ac12074ef9fdde2f985a457ff77319b243aa279
Mohamed Elkhattam <medo20333882@yahoo.com>
2025-05-16 23:47:31 +0300

    minor UI fix

--- APIManagerService/src/main/java/com/example/APIManagerService/controller/ReportController.java
+++ APIManagerService/src/main/java/com/example/APIManagerService/controller/ReportController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.lang.reflect.Array;
-import java.util.AbstractList;
+
 import java.util.ArrayList;
 import java.util.List;
 
