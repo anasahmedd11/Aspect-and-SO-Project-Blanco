9e554b6f59085c2fcc7a042a992f2a43146f5182
Anas Ahmed <anas.a.elyamanyy@gmail.com>
2025-05-17 01:06:27 +0300

    expense controller fix

--- APIManagerService/src/main/java/com/example/APIManagerService/controller/ExpenseController.java
+++ APIManagerService/src/main/java/com/example/APIManagerService/controller/ExpenseController.java
@@ -1,6 +1,9 @@
 package com.example.APIManagerService.controller;
 
+import com.example.APIManagerService.entity.Budgets;
+import com.example.APIManagerService.entity.Categories;
 import com.example.APIManagerService.entity.Expenses;
+import com.example.APIManagerService.service.CategoryService;
 import com.example.APIManagerService.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,16 +11,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/expenses")
 public class ExpenseController {
 
     @Autowired
     private ExpenseService expenseService;
+    private CategoryService categoryService;
 
     @GetMapping("/user/{userId}")
     public String getExpensesPage(@PathVariable Long userId, Model model) {
-        model.addAttribute("expenses", expenseService.getAllExpenses(userId));
+        List<Expenses> expenses = expenseService.getAllExpenses(userId);
+        List<Categories> categories = categoryService.getAllCategories();
+        model.addAttribute("expenses", expenses);
+        model.addAttribute("categories", categories);
         model.addAttribute("newExpense", new Expenses());
         model.addAttribute("userId", userId);
         return "expenses";
