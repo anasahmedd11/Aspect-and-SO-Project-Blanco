3f11a6aa7dc9e638919423b0833c61f7a1f5e83f
Mohamed Elkhattam <medo20333882@yahoo.com>
2025-05-16 16:19:00 +0300

    fixed controller

--- APIManagerService/src/main/java/com/example/APIManagerService/controller/LoginController.java
+++ APIManagerService/src/main/java/com/example/APIManagerService/controller/LoginController.java
@@ -3,16 +3,15 @@ package com.example.APIManagerService.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-
-
-@Controller
+@RestController
 public class LoginController {
 
     @GetMapping("/home")
     public String homePage(Model model) {
-//        model.addAttribute("message", "Welcome to Thymeleaf!");
+        //model.addAttribute("message", "Welcome to Thymeleaf!");
         return "home";
     }
 
