82c118b69aa12b73f4b6700d379d53b22b02c7f8
Ahmed Taha <ahmed.t.taha.25@gmail.com>
2025-05-19 06:59:58 +0300

    Removed transactions

--- APIManagerService/src/main/java/com/example/APIManagerService/controller/HomeController.java
+++ APIManagerService/src/main/java/com/example/APIManagerService/controller/HomeController.java
@@ -5,7 +5,6 @@ import com.example.APIManagerService.entity.Expenses;
 import com.example.APIManagerService.entity.Transactions;
 import com.example.APIManagerService.service.BudgetService;
 import com.example.APIManagerService.service.ExpenseService;
-import com.example.APIManagerService.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,26 +16,22 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    private final TransactionService transactionService;
     private final BudgetService budgetService;
     private final ExpenseService expenseService;
 
     @Autowired
-    public HomeController(TransactionService transactionService,
+    public HomeController(
                           BudgetService budgetService,
                           ExpenseService expenseService) {
-        this.transactionService = transactionService;
         this.budgetService = budgetService;
         this.expenseService = expenseService;
     }
 
     @GetMapping("/home")
     public String getHomePage(@CookieValue("active-user-id") Long userId, Model model) {
-        List<Transactions> transactions = transactionService.getAllTransactions(userId);
         List<Budgets> budgets = budgetService.getAllUserBudgets(userId);
         List<Expenses> expenses = expenseService.getAllExpenses(userId);
 
-        model.addAttribute("transactions", transactions);
         model.addAttribute("budgets", budgets);
         model.addAttribute("expenses", expenses);
         model.addAttribute("userId", userId);
